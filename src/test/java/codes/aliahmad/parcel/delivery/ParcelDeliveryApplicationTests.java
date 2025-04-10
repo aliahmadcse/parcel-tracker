25526f85b3c52109113462380326e7dbb1655b5e
Ali Ahmad <aliahmadcse@gmail.com>
2025-04-10 19:23:33 +0500

    init

---
+++ src/test/java/codes/aliahmad/parcel/delivery/ParcelDeliveryApplicationTests.java
@@ -0,0 +1,15 @@
+package codes.aliahmad.parcel.delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcelDeliveryApplicationTests
+{
+
+  @Test
+  void contextLoads()
+  {
+  }
+
+}
